af5f9d1d67904710841935d1558036beff692f7c
wuttipattubs-oss <wuttipat.tubs@gmail.com>
2026-05-17 17:44:40 +0700

    Create ghote.py
    ...

---
+++ ghote.py
@@ -0,0 +1,16 @@
+number=int(input("enter your grade"))
+if  number>100:
+    print("Over grade")
+elif number >=80 or number==100:
+    print("Excellent")
+elif number >=50:
+    print("Pass")
+    
+else:
+    print("Fail")
+
+number=int(input("Enter your number"))
+# if number %2==0:
+#     print("Even number")
+# else:
+#     print("odd number")
